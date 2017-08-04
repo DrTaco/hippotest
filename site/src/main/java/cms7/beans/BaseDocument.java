1cf46698e2acc5658ee951670fffe4e782accb65
tlouistom@gmail.com <22xp55kv>
2017-08-04 15:04:05 +0200

    initial commit

---
+++ site/src/main/java/cms7/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package cms7.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="test:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
